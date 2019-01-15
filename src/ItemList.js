80a903edd5149d3b4a27ffda2b1ab887db102168
Matt <mattanator94@gmail.com>
2019-01-14 20:17:09 -0800

    Limited displayed todos using .filter

--- src/ItemList.js
+++ src/ItemList.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Item from './Item';
 
 const ItemList = ({ list, removeItem, toggleDone }) => {
-  return list.map(list => (
-    <Item
-      removeItem={removeItem}
-      toggleDone={toggleDone}
-      key={list.id}
-      list={list}
-    />
-  ));
+  return (
+    list
+      .filter((val, index) => index < 10)
+      // Filter limits the number of Items didsplayed on the page. If an Item is deleted it is replaced with the next array item
+      .map(list => (
+        <Item
+          removeItem={removeItem}
+          toggleDone={toggleDone}
+          key={list.id}
+          list={list}
+        />
+      ))
+  );
 };
 
 export default ItemList;
